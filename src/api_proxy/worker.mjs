c3a34dc5d983bb91156489d0abae41a876c610a6
bjss <53896552+bjssliu@users.noreply.github.com>
2025-01-09 20:32:45 +0800

    Update worker.mjs
    修改安全等级

--- src/api_proxy/worker.mjs
+++ src/api_proxy/worker.mjs
@@ -194,6 +194,7 @@ const harmCategory = [
   "HARM_CATEGORY_SEXUALLY_EXPLICIT",
   "HARM_CATEGORY_DANGEROUS_CONTENT",
   "HARM_CATEGORY_HARASSMENT",
+  "HARM_CATEGORY_CIVIC_INTEGRITY",
 ];
 const safetySettings = harmCategory.map(category => ({
   category,
